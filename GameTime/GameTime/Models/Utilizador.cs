36d047b58f3f9da99044e9e90d931237a6a47d6b
Henrique Bernardo <henribernardo@hotmail.com>
2018-09-25 19:22:32 +0100

    Updated Homepage

--- GameTime/GameTime/Models/Utilizador.cs
+++ GameTime/GameTime/Models/Utilizador.cs
@@ -21,8 +21,6 @@ namespace GameTime.Models
         [Required(ErrorMessage = "O {0} é de preenchimento obrigatório!")]
         public string Email { get; set; }
         [Required(ErrorMessage = "O {0} é de preenchimento obrigatório!")]
-        [RegularExpression("[A-ZÍÉÂÁ][a-záéíóúàèìòùâêîôûäëïöüãõç]+(( |'|-| dos | da | de | e | d')[A-ZÍÉÂÁ][a-záéíóúàèìòùâêîôûäëïöüãõç]+){1,3}",
-           ErrorMessage = "O {0} apenas pode conter letras e espaços em branco. Cada palavra começa em Maiúscula, seguida de minúsculas...")]
         public string Nome { get; set; }
         public string Foto { get; set; }
 
